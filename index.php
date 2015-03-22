cc644361b30ed61964bab49d48f0bc92ea882156
ZacBeers <zacbeers@gmail.com>
2015-03-22 11:16:38 -0700

    Initial Commit
    base files

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Pixel Builds</title>
+        <link rel="stylesheet" type="text/css" href="style/base.css">
+        <link rel="stylesheet" type="text/css" href="style/layout.css">
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <a href="http://pixelbuilds.com/"><img src="images/logo.png"><h1>Pixel Builds</h1></a>
+            <span class="cta">Start a Project</span>
+        </header>
+    </body>
+</html>
